c803c1abb751327773c19c7c7184ee0cebf300ce
charan <charan2ravi@gmail.com>
2024-03-11 23:43:48 +0530

    3 sections work replicated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports:[HeaderComponent,RecipeListComponent,RecipesComponent]
+  imports:[HeaderComponent,RecipeListComponent,RecipesComponent,ShoppingListComponent]
 })
 export class AppComponent {
   title = 'recipe-app';
--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ShoppingEditListComponent } from '../shopping-edit-list/shopping-edit-list.component';
+import { Ingredient } from '../../shared/ingredient.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shopping-list',
-  standalone:true,
+  standalone: true,
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
-  imports:[ShoppingEditListComponent]
+  imports: [ShoppingEditListComponent,CommonModule],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 4),
+    new Ingredient('Orange', 6),
+  ];
 }
